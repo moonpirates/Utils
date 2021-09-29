b93a03831fc9b9af3c6266b50818203037879010
Jordi <jordi@jordi.com>
2021-09-29 18:56:42 +0200

    Added Vector constants

---
+++ Utils/src/Math/Vector.cpp
@@ -0,0 +1,20 @@
+#include "Vector.h"
+
+const Utils::Vector2 Utils::Vector2::Zero = { 0.0f, 0.0f };
+const Utils::Vector2 Utils::Vector2::One = { 1.0f, 1.0f };
+const Utils::Vector2 Utils::Vector2::Left = { -1.0f, 0.0f };
+const Utils::Vector2 Utils::Vector2::Right = { 1.0f, 0.0f };
+const Utils::Vector2 Utils::Vector2::Up = { 0.0f, 1.0f };
+const Utils::Vector2 Utils::Vector2::Down = { 0.0f, -1.0f };
+
+const Utils::Vector3 Utils::Vector3::Zero = { 0.0f, 0.0f, 0.0f };
+const Utils::Vector3 Utils::Vector3::One = { 1.0f, 1.0f, 1.0f };
+const Utils::Vector3 Utils::Vector3::Left = { -1.0f, 0.0f, 0.0f };
+const Utils::Vector3 Utils::Vector3::Right = { 1.0f, 0.0f, 0.0f };
+const Utils::Vector3 Utils::Vector3::Up = { 0.0f, 1.0f, 0.0f };
+const Utils::Vector3 Utils::Vector3::Down = { 0.0f, -1.0f, 0.0f };
+const Utils::Vector3 Utils::Vector3::Forward = { 0.0f, 0.0f, 1.0f };
+const Utils::Vector3 Utils::Vector3::Back = { 0.0f, 0.0f, -1.0f };
+
+const Utils::Vector4 Utils::Vector4::Zero = { 0.0f, 0.0f, 0.0f, 0.0f };
+const Utils::Vector4 Utils::Vector4::One = { 1.0f, 1.0f, 1.0f, 1.0f };
--- Utils/src/Math/Vector.h
+++ Utils/src/Math/Vector.h
@@ -10,6 +10,13 @@ namespace Utils
 		float X;
 		float Y;
 
+		const static Vector2 Zero;
+		const static Vector2 One;
+		const static Vector2 Left;
+		const static Vector2 Right;
+		const static Vector2 Up;
+		const static Vector2 Down;
+
 		operator Vector3() const;
 		operator Vector4() const;
 	};
@@ -19,6 +26,15 @@ namespace Utils
 		float X;
 		float Y;
 		float Z;
+
+		const static Vector3 Zero;
+		const static Vector3 One;
+		const static Vector3 Left;
+		const static Vector3 Right;
+		const static Vector3 Up;
+		const static Vector3 Down;
+		const static Vector3 Forward;
+		const static Vector3 Back;
 
 		operator Vector2() const;
 		operator Vector4() const;
@@ -30,6 +46,9 @@ namespace Utils
 		float Y;
 		float Z;
 		float W;
+
+		const static Vector4 Zero;
+		const static Vector4 One;
 
 		operator Vector2() const;
 		operator Vector3() const;
